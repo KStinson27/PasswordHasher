e3f1d67b0d6435c5f84f5c5f9d3a03f8293e7d78
kstinson27 <stinson.krystal327@gmail.com>
2020-11-22 08:39:11 -0500

    initial check in

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace PasswordHash
+{
+    class Program
+    { //Old 
+        /*public static byte[] GetHash(string inputString)
+        {
+            using (HashAlgorithm algorithm = SHA256.Create())
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
+
+        public static string GetHashString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(inputString))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
+        }*/
+
+        static void Main(string[] args)
+        {
+            
+            //string password;
+            //string hashPassword;
+
+            var pH = new PasswordHasher<string>();
+
+            Console.WriteLine(pH.HashPassword(null, "password"));
+            Console.ReadLine();
+
+            
+          
+        }
+    }
+}
